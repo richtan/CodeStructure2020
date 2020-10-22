cead83b2d6a0aaa3d5445f755ac989c6f20be065
Maciej4 <m@maciej.me>
2020-10-22 14:03:41 -0700

    add intake template

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+public class Intake {
+    // One TalonSRX on ID 5 for spinning the intake. One double solenoid for controlling the piston to lift the intake.
+    
+    /**
+    * Lowers the intake using a double solenoid attached to a piston.
+    */
+    public void raiseIntake() {
+        return;
+    }
+
+    /**
+    * Raise the intake using a double solenoid attached to a piston.
+    */
+    public void lowerIntake() {
+        return;
+    }
+
+    /**
+    * Spin intake using PIDF.
+    */
+    public void spinIntakePIDF(double speedGoal) {
+        return;
+    }
+
+    /**
+    * Spin the inake motor at speed.
+    */
+    public void spinIntake(double speed) {
+        return;
+    }
+}
