40dc2faef47311a690b0d5c2ad3cb9420af54a8d
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2020-10-23 08:33:25 -0700

    Updated Climber Code Structure

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,61 +1,102 @@
 package frc.robot.subsystems;
+import com.revrobotics.*;
+import frc.robot.util.PIDF;
 
 public class Climber {
     //Climber has 4 NEOs. IDs 1,2 are for the telescope. IDs 3,4 are for the climbing spool.
+
+    //motors
+    CANSparkMax telescopeNEO1 = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax telescopeNEO2 = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax spoolNEO1 = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax spoolNEO2 = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    //encoders (both spool/telescope motors should be at the same position so we only need one)
+    CANEncoder tele1Encoder = telescopeNEO1.getEncoder();
+    CANEncoder spool1Encoder = spoolNEO1.getEncoder();
+
+    //motors move the same, so these will just follow. Only need to use spoolNEO1 or telescopeNEO1
+    telescopeNEO2.follow(telescopeNEO1);
+    spoolNEO2.follow(spoolNEO1);
+
+    //vars for PIDF
+    long currentTime;
+    long previousTime = 0;
+    double timeFrame;
+    double telescopeNextInput;
+    double spoolNextInput;
+    double telescopeGoalPosition;
+    double spoolGoalPosition;
+
+    //TODO: Set PIDF factors
+    PIDF telePIDF = new PIDF(1, 0, 0);
+    PIDF spoolPIDF = new PIDF(1, 0, 0);
+
+
+    public void loop() {
+        currentTime = System.currentTimeMillis();
+        timeFrame = (currentTime - previousTime)/1000.0; //1000 to convert to seconds, also casts as double (I think)
+        previousTime = currentTime;
+        telescopeNextInput = telePIDF.update(telescopeGoalPosition, getTelescopeHeight(), timeFrame);
+        spoolNextInput = spoolPIDF.update(spoolGoalPosition, getSpoolPosition(), timeFrame);
+        telescopeNEO1.set(telescopeNextInput);
+        spoolNEO1.set(spoolNextInput);
+    }
+
 
     /**
     * Uses moveTelescopePIDF to move the telescope to the upper position: 1000.
     */
     public void raiseTelescope() {
-        return;
+        moveTelescopePIDF(1000.0);
     }
 
     /**
     * Uses moveTelescopePIDF to move the telescope to the lower position: 0.
     */
     public void lowerTelescope() {
-        return;
+        moveTelescopePIDF(0.0);
     }
 
     /**
     * Uses PIDF to move the telescope to a given position.
     */
     public void moveTelescopePIDF(double goalPosition) {
-        return;
+        telescopeGoalPosition = goalPosition;
     }
 
     /**
     * Sets the powers on the two telescope motors.
     */
-    public void setTelescopePower() {
-        return;
+    public void setTelescopePower(double speed) {
+        telescopeNEO1.set(speed);
     }
 
     /**
-    * Returns the height of the telescope in encoder ticks.
+    * Returns the height of the telescope in motor rotations.
     */
     public double getTelescopeHeight() {
-        return 0.0;
+        return tele1Encoder.getPosition();
     }
 
     /**
     * Uses moveSpoolPIDF to retract the spool to the fully retracted position: 1000.
     */
     public void spoolIn() {
-        return;
+        moveSpoolPIDF(1000.0);
     }
 
     /**
     * Uses PIDF to move the telescope to a given position.
     */
     public void moveSpoolPIDF(double goalPosition) {
-        return;
+        spoolGoalPosition = goalPosition;
     }
 
     /**
-    * Returns the position of the spool in encoder ticks.
+    * Returns the position of the spool in motor rotations.
     */
     public double getSpoolPosition() {
-        return 0.0;
+        return spool1Encoder.getPosition();
     }
 }
